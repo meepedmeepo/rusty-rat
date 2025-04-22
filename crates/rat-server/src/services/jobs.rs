c2e8e966e65445eacf16df8d119c4286b9a580e0
meepedmeepo <meepedmeepo@gmail.com>
2025-04-22 19:36:16 +0000

    Added service function for updating a job result, and added server settings.toml to gitignore as it may be used in future as it may contain references to secrets.

--- crates/rat-server/src/services/jobs.rs
+++ crates/rat-server/src/services/jobs.rs
@@ -1,6 +1,9 @@
 use anyhow::Error;
 use chrono::Utc;
-use common::schemas::{DatabaseError, UpdateJobResult};
+use common::{
+    cryptographic_functions::encryption::CryptographyError,
+    schemas::{CreateJob, DatabaseError, UpdateJobResult},
+};
 use uuid::Uuid;
 
 use super::{Job, Service};
@@ -41,15 +44,43 @@ impl Service {
         }
     }
 
-    pub async fn update_job_result(&self, input : UpdateJobResult) -> Result<(), Error> {
+    ///Verifies if the signature of the job result is valid and if it is updates the database to contain the result of the job
+    pub async fn update_job_result(&self, input: UpdateJobResult) -> Result<(), Error> {
         let mut job = self.repo.find_job_by_id(&self.db, input.job_id).await?;
-        let agent = self.repo.find_agent_by_id(&self.db, job.agent_id).await?:
+        let agent = self.repo.find_agent_by_id(&self.db, job.agent_id).await?;
 
         let mut job_result_buffer = input.job_id.as_bytes().to_vec();
         job_result_buffer.append(&mut agent.id.as_bytes().to_vec());
-        job_result_buffer.append(&mut input.encrypted_job_result.clone() );
+        job_result_buffer.append(&mut input.encrypted_job_result.clone());
         job_result_buffer.append(&mut input.ephemeral_public_key.to_vec());
         job_result_buffer.append(&mut input.nonce.to_vec());
+
+        let sig = ed25519_dalek::Signature::try_from(&input.signature[0..64])?;
+        let agent_identity_public_key =
+            ed25519_dalek::VerifyingKey::try_from(&agent.identity_public_key[0..32])?;
+
+        match agent_identity_public_key.verify_strict(&job_result_buffer, &sig) {
+            Err(err) => return Err(CryptographyError::SignatureInvalid(err).into()),
+
+            Ok(_) => {}
+        }
+
+        job.encrypted_result = Some(input.encrypted_job_result);
+        job.result_ephemeral_public_key = Some(input.ephemeral_public_key.to_vec());
+        job.result_nonce = Some(input.nonce.to_vec());
+        job.result_signature = Some(input.signature);
+        self.repo.update_job(&self.db, &job.into()).await
+    }
+
+    pub async fn create_job(&self, input: &CreateJob) -> Result<Job, Error> {
+        let mut job_buffer = input.id.as_bytes().to_vec();
+        job_buffer.append(&mut input.agent_id.as_bytes().to_vec());
+        job_buffer.append(&mut input.encrypted_job.clone());
+        job_buffer.append(&mut input.ephemeral_public_key.to_vec());
+        job_buffer.append(&mut input.nonce.to_vec());
+
+        let sig = ed25519_dalek::Signature::try_from(&input.signature[0..64])?;
+
         todo!()
     }
 }
